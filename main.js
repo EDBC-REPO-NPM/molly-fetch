835f05689d3e64424657f9eb7f6c2d82f1cdf770
becerraenmanuel <BecerraCEnmanuelD@gmail.com>
2022-09-16 15:42:47 -0400

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+const {Buffer} = require('buffer');
 const https = require('https');
 const http = require('http');
 const url = require('url');
@@ -6,68 +6,114 @@ const fs = require('fs');
 
 /*-------------------------------------------------------------------------------------------------*/
 
-const size = Math.pow(10,6) * 3;
+function parseProxy( _args ){
 
-/*-------------------------------------------------------------------------------------------------*/
+    let opt,prot;
 
-function parseURL( ..._args ){
+    if( _args[1]?.proxy ) {
 
-    if( typeof _args[0] == 'string' ){
-        const protocol  = (/https/gi).test(_args[0]) ? https : http;
-        const options = url.parse(_args[0]);
+        opt = new Object();
+        prot = _args[1]?.proxy.protocol=='https' ? https : http;
+        opt.host  = _args[1]?.proxy.host;
+        opt.port  = _args[1]?.proxy.port;
+        opt.path  = _args[1].proxy?.path || _args[1]?.url;
+        opt.agent = new prot.Agent( _args[1]?.agent || 
+            { rejectUnauthorized: false }
+        );
 
-        options.agent   = new protocol.Agent({ rejectUnauthorized: false });
-        options.port    = (/https/gi).test(_args[0]) ? 443 : 80;
-        options.method  = _args[1]?.method || 'GET';
-        options.headers =_args[1]?.headers || {};
-        options.body    =_args[1]?.body || null;
-        return options;
+    } else if( _args[0]?.proxy ){
+
+        opt = new Object();
+        prot = _args[0].proxy.protocol=='https' ? https : http;
+        opt.host  = _args[0].proxy.host; 
+        opt.port  = _args[0].proxy.port;
+        opt.path  = _args[0].proxy?.path || _args[0]?.url;
+        opt.agent = new prot.Agent( _args[0]?.agent || 
+            { rejectUnauthorized: false }
+        );
 
     } else {
-        const protocol  = (/https/gi).test(_args[0]?.url) ? https : http;
-        const options = url.parse(_args[0]?.url);
 
-        options.agent   = new protocol.Agent({ rejectUnauthorized: false });
-        options.port    = (/https/gi).test(_args[0]?.url) ? 443 : 80;
-        options.method  = _args[0]?.method || 'GET';
-        options.headers =_args[0]?.headers || {};
-        options.body    =_args[0]?.body || null; 
-        return options;
+        opt = url.parse( _args[0]?.url || _args[0] );
+        prot = (/https/gi).test( _args[0]?.url || _args[0] ) ? https : http;
+        opt.port = (/https/gi).test( _args[0]?.url || _args[0] ) ? 443 : 80;
+        opt.agent = new prot.Agent( 
+            _args[1]?.agent || _args[0]?.agent || { rejectUnauthorized: false }
+        );
 
     }
 
+    return { opt,prot };
+}
+
+function parseURL( _args ){ 
+    
+    const { opt,prot } = parseProxy( _args );
+    opt.body     = _args[1]?.body || _args[0]?.body || null; 
+    opt.headers  = _args[1]?.headers || _args[0]?.headers || {};
+    opt.method   = _args[1]?.method || _args[0]?.method || 'GET';
+    opt.redirec  = _args[1]?.redirect || _args[0]?.redirect || false; 
+    opt.timeout  = _args[1]?.timeout || _args[0]?.timeout || 60 * 1000 ;
+    opt.response = _args[1]?.responseType || _args[0]?.responseType || 'text';
+    process.chunkSize = _args[1]?.chunkSize || _args[0].chunkSize || Math.pow(10,6) * 3;
+
+    console.log( _args[1] || _args[0] ,opt.response )
+
+    return { opt,prot };
 }
 
 function parseRange( range ){
+    const size = process.chunkSize;
     const interval = range.match(/\d+/gi);
     const chunk = Number(interval[0])+size;
     return interval[1] ? range : range+chunk;
 }
 
+function body( stream ){
+    return new Promise((response,reject)=>{
+        const raw = new Array();
+        stream.on('data',(chunk)=>{
+            raw.push(chunk);
+        })
+        stream.on('close',()=>{
+            const data = Buffer.concat(raw);
+            response( data.toString() )
+        })
+    })
+}
+
 /*-------------------------------------------------------------------------------------------------*/
 
-function fetch( ..._args ) {
+function fetch( ..._args ){
     return new Promise((response,reject)=>{
  
-        const opt = parseURL(..._args); 
-        const protocol = opt.port==443 ? https : http;
+        const { opt,prot } = parseURL( _args ); 
 
         if( opt.headers.range ) 
             opt.headers.range = parseRange(opt.headers.range);
-        if(opt.body){
-            opt.headers['Content-Length'] = Buffer.byteLength(opt.body);
+        if( opt.body ){
             opt.headers['Content-Type'] = 'text/plain';
-        } 
-    
-        const req = new protocol.request( opt,(res) => {
-            if( res.headers.location ) return response({
-                url: res.headers.location.replace(/^http.*:\/\//gi,'?href='),
-                headers: res.headers,
-            }); response( res );
+            opt.headers['Content-Length'] = Buffer.byteLength(opt.body);
+        }
+
+        const req = new prot.request( opt,async(res) => {
+            
+            if( res.headers.location ) {
+                const options = typeof _args[0]!='string' ? _args[0] : _args[1];
+                if( !opt.redirec ) return response({ headers: res.headers,
+                    url: res.headers.location,
+                }); else return response(fetch( res.headers.location, options ));
+            }
+            
+            else if( opt.response == 'text' ) res.data = await body(res);
+            if( res.statusCode >= 300 ) return reject( res )
+            else return response( res );
+            
         });
     
         req.on('error',(e)=>{ reject(e) }); 
         if(opt.body) req.write(opt.body);
+        req.setTimeout( opt.timeout );
         req.end();
 
     });    
